6c5f8d48141a6cc069ab0e653bd79ffec94c019c
chrissergio96 <safou.christiansergio@gmail.com>
2025-12-04 15:54:25 +0100

    'cdq'

--- src/Component-cdq/Comite-cdq/Comitecdq.jsx
+++ src/Component-cdq/Comite-cdq/Comitecdq.jsx
@@ -5,14 +5,10 @@ const Textebon = ({ children }) => {
   return (
 
     <div className='boss-comite1'>
-    <h4 className='enfant-text-comite1'>COMITE DE DEVELOPPEMENT DES QUARTIERS (CDQ) DU 2eme ARRONDISSEMENT DE PORT-GENTIL</h4> 
+    <h4 className='enfant-text-comite1'>HISTOIRE DE LA MAIRIE DU 4e ARRONDISSEMENT DE LIBREVILLE</h4> 
     <div className='texte-comite'>
       <p>
-      Le Comité de Développement des Quartiers (CDQ) est
-      un acteur clé dans l'amélioration du cadre de vie de nos quartiers. Chaque CDQ, propre à chaque quartier,travaille en étroite collaboration avec le Chef du dit quartier.
-      Les CDQ, ce sont avant tout des citoyens engagés qui donnent de leur temps pour améliorer leur quartier. 
-      Leur action est essentielle, mais elle ne peut se faire sans vous. En rejoignant votre CDQ, vous pourrez 
-      contribuer activement à façonner votre environnement et à créer un quartier où il fait bon vivre.
+        La Mairie du 4e Arrondissement de Libreville 
       </p>
     </div>
     </div>   
